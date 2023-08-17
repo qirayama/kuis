2f193bc236a97e3ef8912b9cfba87a76a2404038
qirayama <arikuloli@gmail.com>
2023-08-17 08:04:18 +0700

    edit app.js

--- tugas-reactjs/myapp/src/App.js
+++ tugas-reactjs/myapp/src/App.js
@@ -1,17 +1,12 @@
+import React from 'react'
 import Tugas10 from './tugas10/tugas10.js'
 import Tugas11 from './tugas11/tugas11.js'
-import Tugas6 from './tugas6/tugas6.js'
-import Tugas7 from './tugas7/tugas7.js'
-
 import './App.css'
-
-
-
 
 function App() {
   return (
     <>
-      <Tugas10 />
+      {/*<Tugas10 />*/}
       <Tugas11 />
     </>
   )
