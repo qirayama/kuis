5d9e6d9072e7b58d60851abe1099b95fcef2ebf9
qirayama <arikuloli@gmail.com>
2023-08-27 21:44:05 +0700

    fix build resolve for main.jsx

--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import JobContextProvider from './contexts/JobContext.jsx'
+import JobContextProvider from './contexts/JobContext'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import AuthContextProvider from './contexts/authContext.jsx'
+import AuthContextProvider from './contexts/authContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
